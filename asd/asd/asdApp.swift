f8bc7db0a16972989c83ddd2cd0fe0c34df46b8a
ilhamnrfzn <47941517+ilhamnrfzn@users.noreply.github.com>
2021-12-08 05:44:51 +0700

    Add files via upload

---
+++ asd/asd/asdApp.swift
@@ -0,0 +1,17 @@
+//
+//  asdApp.swift
+//  asd
+//
+//  Created by Stormrage on 08/12/21.
+//
+
+import SwiftUI
+
+@main
+struct asdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
